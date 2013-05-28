b0545b5434b49d69c812f1959c79df2ad948b223
João Bernardo Oliveira <jbvsmo@gmail.com>
2013-05-28 16:01:00 -0300

    Comandos do FFmpeg com mais de 1 output

--- cetrio/ffmpeg.py
+++ cetrio/ffmpeg.py
@@ -5,14 +5,36 @@ from config import config
 bin_default = config.get('ffmpeg', 'bin')
 probe = config.get('ffmpeg', 'probe')
 
-def cmd(cmd_input, input, cmd_output, output, add_probe=True, bin=None):
-    """ Build FFmpeg command for a single input and single output
+
+def _input_cmd(cmd_input, input, add_probe=True, bin=None):
+    """ Base of FFmpeg command with a single input.
     """
     args = [bin_default if bin is None else bin]
     args += shlex.split(cmd_input)
     if add_probe:
         args += ['-probesize', probe]
     args += ['-i',  input]
+    return args
+
+
+def cmd(cmd_input, input, cmd_output, output, add_probe=True, bin=None):
+    """ Build FFmpeg command for a single input and single output.
+    """
+    args = _input_cmd(cmd_input, input, add_probe, bin)
     args += shlex.split(cmd_output)
     args.append(output)
+    return args
+
+
+def cmd_outputs(cmd_input, input, base_cmd_output, cmd_output_specific, outputs, add_probe=True, bin=None):
+    """ Build FFmpeg command for multiple outputs but single input.
+    """
+    args = _input_cmd(cmd_input, input, add_probe, bin)
+
+    base_cmd_output =  shlex.split(base_cmd_output)
+
+    for out_cmd, out in zip(cmd_output_specific, outputs):
+        args += base_cmd_output
+        args += shlex.split(out_cmd)
+        args.append(out)
     return args
