73f7fc369ef7295616678a816e37bfa3e3dfb8aa
João Bernardo Oliveira <jbvsmo@gmail.com>
2013-05-29 15:38:56 -0300

    - Leitura de arquivo local para sobreescrever configurações globais.  - Adicionado no .gitignore

--- cetrio/config.py
+++ cetrio/config.py
@@ -8,10 +8,23 @@ try:
 except ImportError:
     import ConfigParser as configparser
 
-config = configparser.ConfigParser()
 
 dirname = os.path.abspath(os.path.dirname(__file__))
+
+config = configparser.ConfigParser()
 config.read(os.path.join(dirname, 'cetrio.conf'))
+
+# Update system wide configs with local values.
+_local_config = configparser.ConfigParser()
+try:
+    _local_config.read(os.path.join(dirname, 'local.conf'))
+except Exception:
+    pass
+else:
+    for sec in _local_config.sections():
+        for k,v in _local_config.items(sec):
+            config.set(sec, k, v)
+
 
 def create_dir(name):
     if not os.path.exists(name):
