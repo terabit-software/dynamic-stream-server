cfe6f0ef71f8b530661b045f32ec7bc7b6f28779
João Bernardo Oliveira <jbvsmo@gmail.com>
2013-06-04 11:31:37 -0300

    Uso de interpolação extendida no arquivo de configurações. (Para Python < 3.2 é necessário usar "configparser" do PyPI)

--- cetrio/config.py
+++ cetrio/config.py
@@ -3,19 +3,20 @@
 """
 
 import os
-try:
-    import configparser
-except ImportError:
-    import ConfigParser as configparser
+import configparser
 
+def _parser():
+    return configparser.ConfigParser(
+        interpolation=configparser.ExtendedInterpolation()
+    )
 
 dirname = os.path.abspath(os.path.dirname(__file__))
 
-config = configparser.ConfigParser()
+config = _parser()
 config.read(os.path.join(dirname, 'cetrio.conf'))
 
 # Update system wide configs with local values.
-_local_config = configparser.ConfigParser()
+_local_config = _parser()
 try:
     _local_config.read(os.path.join(dirname, 'local.conf'))
 except Exception:
@@ -23,14 +24,14 @@ except Exception:
 else:
     for sec in _local_config.sections():
         for k,v in _local_config.items(sec):
-            config.set(sec, k, v)
+            config[sec][k] = v
 
 
 def create_dir(name):
     if not os.path.exists(name):
         os.makedirs(name)
 
-create_dir(config.get('cache', 'dir'))
-create_dir(config.get('thumbnail', 'dir'))
-create_dir(config.get('log', 'dir'))
+create_dir(config['cache']['dir'])
+create_dir(config['thumbnail']['dir'])
+create_dir(config['log']['dir'])
 
