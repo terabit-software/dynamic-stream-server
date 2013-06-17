9724c8ca26c30c5692f98cd24c86a2f2d617e76e
Bernardo Vianna <jbvsmo@gmail.com>
2013-06-16 22:46:35 -0300

    - Suporte para dicionário na pseudo Python sintaxe ao carregar dados de arquivo.    É possível criar dicionários e sets com a sintaxe "{...}"  - Carregar strings pode utilizar caracteres de escape  - Possibilidade de colocar aspas duplas entre aspas simples e vice-versa

--- dss/config.py
+++ dss/config.py
@@ -7,7 +7,7 @@ import string
 import ast
 import configparser
 
-
+### Fixing the SectionProxy class from configparser module.
 #noinspection PyCallingNonCallable
 def _section_getattr_replacement(self, attr):
     if not attr.startswith('get'):
@@ -19,21 +19,45 @@ def _section_getattr_replacement(self, attr):
 configparser.SectionProxy.__getattr__ = _section_getattr_replacement
 
 
-def _insert(x, obj):
-    if isinstance(obj, set):
-        obj.add(x)
-    else:
-        obj.append(x)
+class _Pair(object):
+    def __init__(self, a, b):
+        self._data = (a, b)
 
+    __hash__ = None
 
-def _add_element(el, obj, allow_empty=False):
-    if el or allow_empty:
-        try:
-            el = ast.literal_eval(el)
-        except Exception:
-            pass
-        _insert(el, obj)
-    return ''
+    @property
+    def data(self):
+        return self._data
+
+    @classmethod
+    def convert_list(cls, lst):
+        pairs = sum(isinstance(x, cls) for x in lst)
+        if pairs:
+            if pairs != len(lst):
+                print('-->', pairs)
+                raise TypeError('Do not mix dictionary key-value '
+                                'pairs with single values.')
+            return dict(x.data for x in lst)
+        return set(lst)
+
+    _pair_error = 'Cannot create pair without %s element.'
+
+    @classmethod
+    def insert_element(cls, el, obj, allow_empty=False, create_pair=False):
+        if el or allow_empty:
+            try:
+                el = ast.literal_eval(el)
+            except Exception:
+                pass
+            if create_pair:
+                try:
+                    el = cls(obj.pop(), el)
+                except IndexError:
+                    raise TypeError(cls._pair_error % 'first')
+            obj.append(el)
+        elif create_pair:
+            raise TypeError(cls._pair_error % 'second')
+        return '', False
 
 
 def _pseudo_list_load(value):
@@ -45,40 +69,69 @@ def _pseudo_list_load(value):
     continuation = ''
     block_continuation = []
     block_token = {
-        '[': (list, ']'),
-        '(': (list, ')'),
-        '{': (set,  '}'),
+        '[': ']',
+        '(': ')',
+        '{': '}',
     }
-    close_error = [x[1] for x in block_token.values()]
+    block_token_close = {
+        ']': list,
+        ')': tuple,
+        '}': _Pair.convert_list,
+    }
+    next_as_pair = False
 
     for char in value:
         if char == continuation:
-            this = _add_element(this, block, allow_empty=True)
+            this, next_as_pair = _Pair.insert_element(
+                continuation + this + continuation,  # Interpreted as string
+                block,
+                allow_empty=True,
+                create_pair=next_as_pair
+            )
             continuation = ''
             continue
         elif continuation:
             this += char
             continue
         elif char in string.whitespace + ',':
-            this = _add_element(this, block)
+            if next_as_pair and not this:
+                if char == ',':
+                    raise ValueError('Expecting dictionary value, '
+                                     'got "," instead')
+                else:
+                    continue
+            this, next_as_pair = _Pair.insert_element(
+                this, block,
+                create_pair=next_as_pair
+            )
             continue
         elif char in '"\'':
-            continuation = char
+            if not continuation:
+                continuation = char
+                continue
+        elif char in ':':
+            next_as_pair = True
+            this, _ = _Pair.insert_element(this, block, create_pair=False)
+            if not block_continuation or block_continuation[-1] != '}':
+                raise ValueError('Cannot create dictionary without { }')
             continue
         elif char in block_token:
-            type_, close = block_token[char]
-            block = type_()
-            block_continuation.append(close)
-            _insert(block, block_order[-1])
+            block = []
             block_order.append(block)
+            block_continuation.append(block_token[char])
             continue
         elif block_continuation and char in block_continuation[-1]:
-            this = _add_element(this, block)
-            block_order.pop()
-            block_continuation.pop()
+            char = block_continuation.pop()
+            this, next_as_pair = _Pair.insert_element(
+                this, block,
+                create_pair=next_as_pair
+            )
+            converter = block_token_close[char]
+            old_block = converter(block_order.pop())
             block = block_order[-1]
+            block.append(old_block)
             continue
-        elif char in close_error:
+        elif char in block_token_close:
             raise ValueError('Cannot close block: %s' % char)
         this += char
 
@@ -89,7 +142,7 @@ def _pseudo_list_load(value):
         raise ValueError('End of text while reading block. Missing: %s' %
                          ' '.join(reversed(block_continuation)))
     if this:
-        _add_element(this, block)
+        _Pair.insert_element(this, block, create_pair=next_as_pair)
 
     return tokens
 
