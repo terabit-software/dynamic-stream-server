f60e21fef43606fcd8fb7ac61df01f219e4cb02e
João Bernardo Oliveira <jbvsmo@gmail.com>
2013-09-27 14:07:08 -0300

    Moved stream control handler to new package

--- dss/web.py
+++ dss/web.py
@@ -2,54 +2,8 @@ import time
 import tornado.ioloop
 import tornado.web
 
-from . import video
 from .config import config
-from .tools import show
-
-
-class VideoStreamHandler(tornado.web.RequestHandler):
-    timeout = config.getint('local', 'http_client_timeout')
-    max_timeout = config.getint('local', 'http_client_timeout_max')
-    min_timeout = config.getint('local', 'http_client_timeout_min')
-
-    def handle_start(self, id):
-        try:
-            video.Video.start(id)
-        except KeyError:
-            return 404
-
-    def handle_http(self, id):
-        try:
-            timeout = int(self.path_args[2])
-        except (IndexError, ValueError):
-            timeout = self.timeout
-
-        timeout = max(timeout, self.min_timeout)
-        timeout = min(timeout, self.max_timeout)
-
-        try:
-            video.Video.start(id, http_wait=timeout)
-        except KeyError:
-            return 404
-
-    def handle_stop(self, id):
-        video.Video.stop(id)
-
-    def get(self, *args, **kw):
-        id, action = args[:2]
-
-        try:
-            handle = getattr(self, 'handle_' + action)
-            code = handle(id)
-        except Exception as e:
-            show('Error on request handling: %r' % e)
-            self.set_status(500)
-        else:
-            self.set_status(code or 200)
-        finally:
-            self.finish()
-
-    post = get
+from .web_handlers import stream_control
 
 
 class Server(object):
@@ -57,7 +11,7 @@ class Server(object):
     port = config.getint('local', 'port')
 
     application = tornado.web.Application([
-        (r"/stream/(.*?)/(start|stop|http)/?(\d*)", VideoStreamHandler),
+        (r"/stream/(.*?)/(start|stop|http)/?(\d*)", stream_control.StreamControl),
     ])
 
     tcp_retry = 10  # seconds
---
+++ dss/web_handlers/stream_control.py
@@ -0,0 +1,51 @@
+import tornado.web
+
+from .. import video
+from ..tools import show
+
+from ..config import config
+
+
+class StreamControl(tornado.web.RequestHandler):
+    timeout = config.getint('local', 'http_client_timeout')
+    max_timeout = config.getint('local', 'http_client_timeout_max')
+    min_timeout = config.getint('local', 'http_client_timeout_min')
+
+    def handle_start(self, id):
+        try:
+            video.Video.start(id)
+        except KeyError:
+            return 404
+
+    def handle_http(self, id):
+        try:
+            timeout = int(self.path_args[2])
+        except (IndexError, ValueError):
+            timeout = self.timeout
+
+        timeout = max(timeout, self.min_timeout)
+        timeout = min(timeout, self.max_timeout)
+
+        try:
+            video.Video.start(id, http_wait=timeout)
+        except KeyError:
+            return 404
+
+    def handle_stop(self, id):
+        video.Video.stop(id)
+
+    def get(self, *args, **kw):
+        id, action = args[:2]
+
+        try:
+            handle = getattr(self, 'handle_' + action)
+            code = handle(id)
+        except Exception as e:
+            show('Error on request handling: %r' % e)
+            self.set_status(500)
+        else:
+            self.set_status(code or 200)
+        finally:
+            self.finish()
+
+    post = get
