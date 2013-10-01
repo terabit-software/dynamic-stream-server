15801bea3a219ec0d4404df627f9805c499710b9
João Bernardo Oliveira <jbvsmo@gmail.com>
2013-10-01 18:49:42 -0300

    Base class for stats objects.

--- dss/stats.py
+++ dss/stats.py
@@ -1,36 +1,51 @@
 from .tools import thread
 
 
+class Stats(thread.LockedObject):
+    __locked_properties__ = ('total', 'measures')
+
+    def __init__(self, total=0, measure=0):
+        super(Stats, self).__init__()
+        self.total = total
+        self.measure = measure
+
+    def result(self):
+        """ The element being measured divided by the total of occurrences.
+            This should always be a value between 0 and 1 (inclusive).
+        """
+        try:
+            return self.measure / self.total
+        except ZeroDivisionError:
+            return 0.
+
+
+class CountStats(Stats):
+    __locked_properties__ = ('count',)
+
+    def __init__(self, total=0, error_count=0):
+        super(CountStats, self).__init__(total)
+        self.count = error_count
+
+    @property
+    def measure(self):
+        return self.total - self._count
+
+    @measure.setter
+    def measure(self, value):
+        return
+
+    def inc(self, error):
+        self.total += 1
+        if error:
+            self.count += 1
+
+
 class StreamStats(object):
-    class Stats(object):
-        def __init__(self, total=0, error_count=0):
-            self._total = total
-            self._count = error_count
-            self._lock = thread.Lock()
-
-        def inc(self, error):
-            with self._lock:
-                self._total += 1
-                if error:
-                    self._count += 1
-
-        def error(self):
-            try:
-                return self._count / self._total
-            except ZeroDivisionError:
-                return 0.
-
-        def ok(self):
-            return 1 - self.error()
-
-    class TimeStats(Stats):
-        pass
-
     def __init__(self):
-        self.thumbnail = self.Stats()
+        self.thumbnail = CountStats()
 
-    def metric(self):
-        # TODO: add other metrics
+    def metric(self, percent=True):
+        mult = 100 if percent else 1
         return {
-            'thumbnail': self.thumbnail.ok() * 100,
+            'thumbnail': self.thumbnail.result() * mult,
         }
