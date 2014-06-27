9e5afbb30da983726ed79ca46f3a39d0b235b96b
João Bernardo Oliveira <jbvsmo@gmail.com>
2014-06-27 13:26:17 -0300

    Dictionary with "attribute" syntax

--- dss/tools/__init__.py
+++ dss/tools/__init__.py
@@ -7,3 +7,22 @@ PRINT_LOCK = thread.Lock()
 def show(*args, **kw):
     with PRINT_LOCK:
         print(*args, **kw)
+
+
+class DictObj(dict):
+    """ Dictionary with attribute syntax to get, set and delete items.
+    """
+    def __getattr__(self, item):
+        try:
+            return self[item]
+        except KeyError:
+            raise AttributeError(item)
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+    def __delattr__(self, item):
+        try:
+            del self[item]
+        except KeyError:
+            raise AttributeError(item)
