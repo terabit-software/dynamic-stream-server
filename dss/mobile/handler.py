d2df8cad874ef7bee72247255ebb5480c43d93c0
João Bernardo Oliveira <jbvsmo@gmail.com>
2014-09-15 16:35:24 -0300

    Create new function to handle metadata. Other parts of the program should move from "send_data" "write_data" using the new "as_metadata" argument.

--- dss/mobile/handler.py
+++ dss/mobile/handler.py
@@ -286,12 +286,20 @@ class MediaHandler(socketserver.BaseRequestHandler, object):
             return None, None
         return typ, payload
 
-    def send_data(self, type, content):
+    @classmethod
+    def build_metadata(cls, type, content):
         data = {'type': type.name, 'content': content}
-        data = json.dumps(data).encode('utf-8')
-        self.write_data(DataContent.metadata, data)
+        return json.dumps(data).encode('utf-8')
 
-    def write_data(self, data_type, data):
+    def send_data(self, type, content):
+        # Deprecated. Use write_data directly
+        self.write_data(type, content, as_metadata=True)
+
+    def write_data(self, data_type, data, as_metadata=False):
+        if as_metadata:
+            data = self.build_metadata(data_type, data)
+            data_type = DataContent.metadata
+
         header = struct.pack('!BI', data_type.value, len(data))
         self.request.sendall(header + data)
 
